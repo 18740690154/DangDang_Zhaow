cbb0fef83eeefc4af33d52b7e8972de8a41e16ea
zhaowei <18740690154@163.com>
2020-04-15 11:40:37 +0800

    为GitHub测试类添加测试方法

--- src/test/java/cn/baizhi/test/TestGitHub.java
+++ src/test/java/cn/baizhi/test/TestGitHub.java
@@ -1,7 +1,14 @@
 package cn.baizhi.test;
 
+import org.junit.Test;
+
 public class TestGitHub {
     public static void main(String[] args) {
         System.out.println("args = " + args);
+    }
+
+    @Test
+    public void test(){
+
     }
 }
